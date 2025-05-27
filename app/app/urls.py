5a5f88b58b913036dc9e1b142d5c941077d2dccd
Vadim Sedushkin <sedushkin27@gmail.com>
2025-05-27 13:53:11 +0300

    add page policy and style for this page.

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,10 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from main.views import index, about
+from main.views import index, about, conditioning_policy
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('about-me/', about, name='about')
+    path('about-me/', about, name='about'),
+    path('conditioning-policy/', conditioning_policy, name='conditioning_policy')
 ]
