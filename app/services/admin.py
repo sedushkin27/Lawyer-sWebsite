40eef2d3c7805c6d57dc689f122996d547b68c87
Vadim Sedushkin <sedushkin27@gmail.com>
2025-05-28 21:27:19 +0300

    Created models

--- app/services/admin.py
+++ app/services/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from services.models import Services, ServiceSection, SectionItem
+
+# admin.site.register(Services)
+admin.site.register(ServiceSection)
+admin.site.register(SectionItem)
+
+@admin.register(Services)
+class ServicesAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
--- app/services/models.py
+++ app/services/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
 # Create your models here.
+class Services(models.Model):
+
+    title = models.CharField(("Назва"), max_length=50, unique=True)
+    slug = models.SlugField(('Url'), unique=True, blank=True, null=True)
+    image = models.ImageField(("Зображення"), upload_to='services/service_images', blank=True, null=True)
+    price = models.DecimalField(("Ціна"), max_digits=7, decimal_places=2, default=0.00)
+    description = models.TextField(("Опис"), blank=True, null=True)
+
+    class Meta:
+        db_table = 'service'
+        verbose_name = ("послугу")
+        verbose_name_plural = ("Послуги")
+
+    def __str__(self):
+        return self.title
+
+    # def get_absolute_url(self):
+    #     return reverse("Services_detail", kwargs={"pk": self.slug})
+
+class ServiceSection (models.Model):
+
+    service = models.ForeignKey(Services, related_name=('sections'), verbose_name=("До якої послуги відноситися"), on_delete=models.CASCADE)
+    title = models.CharField(("Заголовок блоку"), max_length=50, unique=True)
+
+    class Meta:
+        db_table = 'serviceSection'
+        verbose_name = "блок з інформацією"
+        verbose_name_plural = "Блоки з інформацією"
+
+    def __str__(self):
+        return self.title
+
+class SectionItem(models.Model):
+    section = models.ForeignKey(ServiceSection, on_delete=models.CASCADE, verbose_name=("До якої блоку відноситися"), related_name=('items'))
+    text = models.TextField("Елемент списку")
+
+    class Meta:
+        db_table = 'sectionItem'
+        verbose_name = "пункт в блок"
+        verbose_name_plural = "Пункти в блоці"
+
+    def __str__(self):
+        return self.text
